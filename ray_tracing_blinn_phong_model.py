34b19fff5b19ca486763c981177a01fe319ef27a
Mingrui <mingrui.zhang@taichi.graphics>
2021-11-01 22:16:31 +0800

    add blinn phong model

---
+++ ray_tracing_blinn_phong_model.py
@@ -0,0 +1,267 @@
+import taichi as ti
+import numpy as np
+import argparse
+ti.init(arch=ti.gpu)
+
+PI = 3.14159265
+
+# Canvas
+aspect_ratio = 1.0
+image_width = 800
+image_height = int(image_width / aspect_ratio)
+canvas = ti.Vector.field(3, dtype=ti.f32, shape=(image_width, image_height))
+
+
+# Rendering parameters
+samples_per_pixel = 4
+max_depth = 10
+
+@ti.data_oriented
+class Ray:
+    def __init__(self, origin, direction):
+        self.origin = origin
+        self.direction = direction
+    def at(self, t):
+        return self.origin + t * self.direction
+
+@ti.data_oriented
+class Sphere:
+    def __init__(self, center, radius, material, color):
+        self.center = center
+        self.radius = radius
+        self.material = material
+        self.color = color
+
+    @ti.func
+    def hit(self, ray, t_min=0.001, t_max=10e8):
+        oc = ray.origin - self.center
+        a = ray.direction.dot(ray.direction)
+        b = 2.0 * oc.dot(ray.direction)
+        c = oc.dot(oc) - self.radius * self.radius
+        discriminant = b * b - 4 * a * c
+        is_hit = False
+        front_face = False
+        root = 0.0
+        hit_point =  ti.Vector([0.0, 0.0, 0.0])
+        hit_point_normal = ti.Vector([0.0, 0.0, 0.0])
+        if discriminant > 0:
+            sqrtd = ti.sqrt(discriminant)
+            root = (-b - sqrtd) / (2 * a)
+            if root < t_min or root > t_max:
+                root = (-b + sqrtd) / (2 * a)
+                if root >= t_min and root <= t_max:
+                    is_hit = True
+            else:
+                is_hit = True
+        if is_hit:
+            hit_point = ray.at(root)
+            hit_point_normal = (hit_point - self.center) / self.radius
+            # Check which side does the ray hit, we set the hit point normals always point outward from the surface
+            if ray.direction.dot(hit_point_normal) < 0:
+                front_face = True
+            else:
+                hit_point_normal = -hit_point_normal
+        return is_hit, root, hit_point, hit_point_normal, front_face, self.material, self.color
+
+@ti.data_oriented
+class Hittable_list:
+    def __init__(self):
+        self.objects = []
+    def add(self, obj):
+        self.objects.append(obj)
+    def clear(self):
+        self.objects = []
+
+    @ti.func
+    def hit(self, ray, t_min=0.001, t_max=10e8):
+        closest_t = t_max
+        is_hit = False
+        front_face = False
+        hit_point = ti.Vector([0.0, 0.0, 0.0])
+        hit_point_normal = ti.Vector([0.0, 0.0, 0.0])
+        color = ti.Vector([0.0, 0.0, 0.0])
+        material = 1
+        for index in ti.static(range(len(self.objects))):
+            is_hit_tmp, root_tmp, hit_point_tmp, hit_point_normal_tmp, front_face_tmp, material_tmp, color_tmp =  self.objects[index].hit(ray, t_min, closest_t)
+            if is_hit_tmp:
+                closest_t = root_tmp
+                is_hit = is_hit_tmp
+                hit_point = hit_point_tmp
+                hit_point_normal = hit_point_normal_tmp
+                front_face = front_face_tmp
+                material = material_tmp
+                color = color_tmp
+        return is_hit, hit_point, hit_point_normal, front_face, material, color
+
+@ti.data_oriented
+class Camera:
+    def __init__(self, fov=60, aspect_ratio=1.0):
+        # Camera parameters
+        self.lookfrom = ti.Vector.field(3, dtype=ti.f32, shape=())
+        self.lookat = ti.Vector.field(3, dtype=ti.f32, shape=())
+        self.vup = ti.Vector.field(3, dtype=ti.f32, shape=())
+        self.fov = fov
+        self.aspect_ratio = aspect_ratio
+
+        self.cam_lower_left_corner = ti.Vector.field(3, dtype=ti.f32, shape=())
+        self.cam_horizontal = ti.Vector.field(3, dtype=ti.f32, shape=())
+        self.cam_vertical = ti.Vector.field(3, dtype=ti.f32, shape=())
+        self.cam_origin = ti.Vector.field(3, dtype=ti.f32, shape=())
+        self.reset()
+
+    @ti.kernel
+    def reset(self):
+        self.lookfrom[None] = [0.0, 1.0, -5.0]
+        self.lookat[None] = [0.0, 1.0, -1.0]
+        self.vup[None] = [0.0, 1.0, 0.0]
+        theta = self.fov * (PI / 180.0)
+        half_height = ti.tan(theta / 2.0)
+        half_width = self.aspect_ratio * half_height
+        self.cam_origin[None] = self.lookfrom[None]
+        w = (self.lookfrom[None] - self.lookat[None]).normalized()
+        u = (self.vup[None].cross(w)).normalized()
+        v = w.cross(u)
+        self.cam_lower_left_corner[None] = ti.Vector([-half_width, -half_height, -1.0])
+        self.cam_lower_left_corner[
+            None] = self.cam_origin[None] - half_width * u - half_height * v - w
+        self.cam_horizontal[None] = 2 * half_width * u
+        self.cam_vertical[None] = 2 * half_height * v
+
+    @ti.func
+    def get_ray(self, u, v):
+        return Ray(self.cam_origin[None], self.cam_lower_left_corner[None] + u * self.cam_horizontal[None] + v * self.cam_vertical[None] - self.cam_origin[None])
+
+@ti.func
+def rand3():
+    return ti.Vector([ti.random(), ti.random(), ti.random()])
+
+
+@ti.func
+def random_in_unit_sphere():
+    p = 2.0 * rand3() - ti.Vector([1, 1, 1])
+    while (p.norm() >= 1.0):
+        p = 2.0 * rand3() - ti.Vector([1, 1, 1])
+    return p
+
+
+@ti.kernel
+def render():
+    for i, j in canvas:
+        u = (i + ti.random()) / image_width
+        v = (j + ti.random()) / image_height
+        color = ti.Vector([0.0, 0.0, 0.0])
+        for n in range(samples_per_pixel):
+            ray = camera.get_ray(u, v)
+            color += ray_color(ray)
+        color /= samples_per_pixel
+        canvas[i, j] += color
+
+# @ti.func
+# def ray_color(ray):
+#     default_color = ti.Vector([1.0, 1.0, 1.0])
+#     scattered_origin = ray.origin
+#     scattered_direction = ray.direction
+#     for n in range(max_depth):
+#         is_hit, hit_point, hit_point_normal, front_face, material, color = scene.hit(Ray(scattered_origin, scattered_direction))
+#         if is_hit:
+#             if front_face:
+#                 target = hit_point + hit_point_normal + random_in_unit_sphere()
+#                 scattered_direction = target - hit_point
+#                 scattered_origin = hit_point
+#             # else:
+#             #     default_color = ti.Vector([0.5, 0.0, 0.0])
+#             default_color *= color
+#     return default_color
+
+@ti.func
+def to_light_source(hit_point, light_source):
+    return light_source - hit_point
+
+# Blinn–Phong reflection model
+@ti.func
+def ray_color(ray):
+    default_color = ti.Vector([1.0, 1.0, 1.0])
+    scattered_origin = ray.origin
+    scattered_direction = ray.direction
+    is_hit, hit_point, hit_point_normal, front_face, material, color = scene.hit(Ray(scattered_origin, scattered_direction))
+    if is_hit:
+        if material == 0:
+            default_color = color
+        else:
+            if front_face:
+                target = hit_point + hit_point_normal + random_in_unit_sphere()
+                scattered_direction = target - hit_point
+                scattered_origin = hit_point
+            hit_point_to_source = to_light_source(hit_point, ti.Vector([0, 5.4, -1]))
+
+            # Diffuse light
+            default_color = color * max(hit_point_to_source.dot(hit_point_normal) / (hit_point_to_source.norm() * hit_point_normal.norm()), 0.0)
+
+            intensity = 0.0
+            if material == 2 or material == 3 or material == 4:
+                # Specular light
+                H = (-(ray.direction.normalized()) + hit_point_to_source.normalized()).normalized()
+                N_dot_H = max(H.dot(hit_point_normal.normalized()), 0.0)
+                intensity = ti.pow(N_dot_H, 10)
+
+            if material == 4:
+                default_color *= 0.5
+                intensity *= 0.5
+
+            default_color += intensity * color
+
+    return default_color
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Naive Ray Tracing')
+    parser.add_argument(
+        '--max_depth', type=int, default=10, help='max depth (default: 10)')
+    parser.add_argument(
+        '--samples_per_pixel', type=int, default=4, help='samples_per_pixel  (default: 4)')
+    args = parser.parse_args()
+
+    max_depth = args.max_depth
+    samples_per_pixel = args.samples_per_pixel
+    scene = Hittable_list()
+
+    # Light source
+    scene.add(Sphere(center=ti.Vector([0, 5.4, -1]), radius=3.0, material=0, color=ti.Vector([10.0, 10.0, 10.0])))
+    # Ground
+    scene.add(Sphere(center=ti.Vector([0, -100.5, -1]), radius=100.0, material=1, color=ti.Vector([0.8, 0.8, 0.8])))
+    # ceiling
+    scene.add(Sphere(center=ti.Vector([0, 102.5, -1]), radius=100.0, material=1, color=ti.Vector([0.8, 0.8, 0.8])))
+    # back wall
+    scene.add(Sphere(center=ti.Vector([0, 1, 101]), radius=100.0, material=1, color=ti.Vector([0.8, 0.8, 0.8])))
+    # right wall
+    scene.add(Sphere(center=ti.Vector([-101.5, 0, -1]), radius=100.0, material=1, color=ti.Vector([0.6, 0.0, 0.0])))
+    # left wall
+    scene.add(Sphere(center=ti.Vector([101.5, 0, -1]), radius=100.0, material=1, color=ti.Vector([0.0, 0.6, 0.0])))
+
+    # Diffuse ball
+    scene.add(Sphere(center=ti.Vector([0, -0.2, -1.5]), radius=0.3, material=1, color=ti.Vector([0.8, 0.3, 0.3])))
+    # Metal ball
+    scene.add(Sphere(center=ti.Vector([-0.8, 0.2, -1]), radius=0.7, material=2, color=ti.Vector([0.6, 0.8, 0.8])))
+    # Glass ball
+    scene.add(Sphere(center=ti.Vector([0.7, 0, -0.5]), radius=0.5, material=3, color=ti.Vector([1.0, 1.0, 1.0])))
+    # Metal ball-2
+    scene.add(Sphere(center=ti.Vector([0.6, -0.3, -2.0]), radius=0.2, material=4, color=ti.Vector([0.8, 0.6, 0.2])))
+
+    camera = Camera()
+    gui = ti.GUI("Ray Tracing", res=(image_width, image_height))
+    canvas.fill(0)
+    cnt = 0
+    while gui.running:
+        # if gui.get_event(ti.GUI.PRESS):
+        #     if gui.event.key == ti.GUI.LMB:
+        #         x, y = gui.get_cursor_pos()
+        #         camera.lookfrom[None][0] = x * 4 - 2
+        #         camera.lookfrom[None][1] = y * 2 - 1
+        #         cnt = 0
+        #         canvas.fill(0)
+        #         camera.reset()
+        #     elif gui.event.key == ti.GUI.ESCAPE:
+        #         exit()
+        render()
+        cnt += 1
+        gui.set_image(np.sqrt(canvas.to_numpy() / cnt))
+        gui.show()
