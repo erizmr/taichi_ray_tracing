ba7e73efbc714cdaf8b10751e315bac0bc5bc004
Mingrui <mingrui.zhang@taichi.graphics>
2021-11-02 16:28:36 +0800

    update whitted style

--- whitted_style_ray_tracing.py
+++ whitted_style_ray_tracing.py
@@ -1,7 +1,8 @@
 import taichi as ti
 import numpy as np
 import argparse
-ti.init(arch=ti.gpu)
+from ray_tracing_models import Ray, Camera, Hittable_list, Sphere, PI, random_in_unit_sphere
+ti.init(arch=ti.cpu, cpu_max_num_threads=5)
 
 PI = 3.14159265
 
@@ -11,138 +12,15 @@ image_width = 800
 image_height = int(image_width / aspect_ratio)
 canvas = ti.Vector.field(3, dtype=ti.f32, shape=(image_width, image_height))
 
+num_of_rays = 10
+scattered_origin_buffer = ti.Vector.field(3, dtype=float, shape=num_of_rays)
+scattered_direction_buffer = ti.Vector.field(3, dtype=float, shape=num_of_rays)
+scattered_buffer_mask = ti.field(dtype=int, shape=num_of_rays)
+
 
 # Rendering parameters
 samples_per_pixel = 4
 max_depth = 10
-
-@ti.data_oriented
-class Ray:
-    def __init__(self, origin, direction):
-        self.origin = origin
-        self.direction = direction
-    def at(self, t):
-        return self.origin + t * self.direction
-
-@ti.data_oriented
-class Sphere:
-    def __init__(self, center, radius, material, color):
-        self.center = center
-        self.radius = radius
-        self.material = material
-        self.color = color
-
-    @ti.func
-    def hit(self, ray, t_min=0.001, t_max=10e8):
-        oc = ray.origin - self.center
-        a = ray.direction.dot(ray.direction)
-        b = 2.0 * oc.dot(ray.direction)
-        c = oc.dot(oc) - self.radius * self.radius
-        discriminant = b * b - 4 * a * c
-        is_hit = False
-        front_face = False
-        root = 0.0
-        hit_point =  ti.Vector([0.0, 0.0, 0.0])
-        hit_point_normal = ti.Vector([0.0, 0.0, 0.0])
-        if discriminant > 0:
-            sqrtd = ti.sqrt(discriminant)
-            root = (-b - sqrtd) / (2 * a)
-            if root < t_min or root > t_max:
-                root = (-b + sqrtd) / (2 * a)
-                if root >= t_min and root <= t_max:
-                    is_hit = True
-            else:
-                is_hit = True
-        if is_hit:
-            hit_point = ray.at(root)
-            hit_point_normal = (hit_point - self.center) / self.radius
-            # Check which side does the ray hit, we set the hit point normals always point outward from the surface
-            if ray.direction.dot(hit_point_normal) < 0:
-                front_face = True
-            else:
-                hit_point_normal = -hit_point_normal
-        return is_hit, root, hit_point, hit_point_normal, front_face, self.material, self.color
-
-@ti.data_oriented
-class Hittable_list:
-    def __init__(self):
-        self.objects = []
-    def add(self, obj):
-        self.objects.append(obj)
-    def clear(self):
-        self.objects = []
-
-    @ti.func
-    def hit(self, ray, t_min=0.001, t_max=10e8):
-        closest_t = t_max
-        is_hit = False
-        front_face = False
-        hit_point = ti.Vector([0.0, 0.0, 0.0])
-        hit_point_normal = ti.Vector([0.0, 0.0, 0.0])
-        color = ti.Vector([0.0, 0.0, 0.0])
-        material = 1
-        for index in ti.static(range(len(self.objects))):
-            is_hit_tmp, root_tmp, hit_point_tmp, hit_point_normal_tmp, front_face_tmp, material_tmp, color_tmp =  self.objects[index].hit(ray, t_min, closest_t)
-            if is_hit_tmp:
-                closest_t = root_tmp
-                is_hit = is_hit_tmp
-                hit_point = hit_point_tmp
-                hit_point_normal = hit_point_normal_tmp
-                front_face = front_face_tmp
-                material = material_tmp
-                color = color_tmp
-        return is_hit, hit_point, hit_point_normal, front_face, material, color
-
-@ti.data_oriented
-class Camera:
-    def __init__(self, fov=60, aspect_ratio=1.0):
-        # Camera parameters
-        self.lookfrom = ti.Vector.field(3, dtype=ti.f32, shape=())
-        self.lookat = ti.Vector.field(3, dtype=ti.f32, shape=())
-        self.vup = ti.Vector.field(3, dtype=ti.f32, shape=())
-        self.fov = fov
-        self.aspect_ratio = aspect_ratio
-
-        self.cam_lower_left_corner = ti.Vector.field(3, dtype=ti.f32, shape=())
-        self.cam_horizontal = ti.Vector.field(3, dtype=ti.f32, shape=())
-        self.cam_vertical = ti.Vector.field(3, dtype=ti.f32, shape=())
-        self.cam_origin = ti.Vector.field(3, dtype=ti.f32, shape=())
-        self.reset()
-
-    @ti.kernel
-    def reset(self):
-        self.lookfrom[None] = [0.0, 1.0, -5.0]
-        self.lookat[None] = [0.0, 1.0, -1.0]
-        self.vup[None] = [0.0, 1.0, 0.0]
-        theta = self.fov * (PI / 180.0)
-        half_height = ti.tan(theta / 2.0)
-        half_width = self.aspect_ratio * half_height
-        self.cam_origin[None] = self.lookfrom[None]
-        w = (self.lookfrom[None] - self.lookat[None]).normalized()
-        u = (self.vup[None].cross(w)).normalized()
-        v = w.cross(u)
-        self.cam_lower_left_corner[None] = ti.Vector([-half_width, -half_height, -1.0])
-        self.cam_lower_left_corner[
-            None] = self.cam_origin[None] - half_width * u - half_height * v - w
-        self.cam_horizontal[None] = 2 * half_width * u
-        self.cam_vertical[None] = 2 * half_height * v
-
-    @ti.func
-    def get_ray(self, u, v):
-        return Ray(self.cam_origin[None], self.cam_lower_left_corner[None] + u * self.cam_horizontal[None] + v * self.cam_vertical[None] - self.cam_origin[None])
-
-@ti.func
-def rand3():
-    return ti.Vector([ti.random(), ti.random(), ti.random()])
-
-
-@ti.func
-def random_in_unit_sphere():
-    p = 2.0 * rand3() - ti.Vector([1, 1, 1])
-    while (p.norm() >= 1.0):
-        p = 2.0 * rand3() - ti.Vector([1, 1, 1])
-    return p
-
 
 @ti.kernel
 def render():
@@ -164,6 +42,41 @@ def to_light_source(hit_point, light_source):
 def reflect(v, normal):
     return v - 2 * v.dot(normal) * normal
 
+@ti.func
+def refract(uv, n, etai_over_etat):
+    cos_theta = min(n.dot(-uv), 1.0)
+    r_out_perp = etai_over_etat * (uv + cos_theta * n)
+    r_out_parallel = -ti.sqrt(abs(1.0 - r_out_perp.dot(r_out_perp))) * n
+    return r_out_perp + r_out_parallel
+
+@ti.func
+def reflectance(cosine, ref_idx):
+    # Use Schlick's approximation for reflectance.
+    r0 = (1 - ref_idx) / (1 + ref_idx)
+    r0 = r0 * r0
+    return r0 + (1 - r0) * pow((1 - cosine), 5)
+
+# @ti.func
+# def add_shadow(hit_point, hit_point_to_source):
+#     ret = 0.0
+#     # Add shadow
+#     is_hit_source, hitted_dielectric_num, is_hitted_non_dielectric = scene.hit_shadow(
+#         Ray(hit_point, hit_point_to_source))
+#     if not is_hit_source:
+#         if is_hitted_non_dielectric:
+#             # Add hard shadow
+#             ret = 0.0
+#         elif hitted_dielectric_num > 0:
+#             # Add soft shadow if the obstacles are dielectric
+#             ret = ti.pow(0.5, hitted_dielectric_num)
+#     return ret
+@ti.func
+def clean_buffer():
+    for i in range(num_of_rays):
+        scattered_buffer_mask[i] = 0
+        scattered_origin_buffer[i] = ti.Vector([0.0, 0.0, 0.0])
+        scattered_direction_buffer[i] = ti.Vector([0.0, 0.0, 0.0])
+
 # Whitted-style ray tracing
 @ti.func
 def ray_color(ray):
@@ -172,7 +85,12 @@ def ray_color(ray):
     diffuse_color_buffer = ti.Vector([0.0, 0.0, 0.0])
     scattered_origin = ray.origin
     scattered_direction = ray.direction
-    ray_traced_color = ti.Vector([0.0, 0.0 , 0.0])
+
+    clean_buffer()
+    scattered_origin_buffer[0] = scattered_origin
+    scattered_direction_buffer[0] = scattered_direction
+    scattered_buffer_mask[0] = 1
+    buffer_pointer = 0
 
     is_hit = False
     front_face = False
@@ -180,45 +98,79 @@ def ray_color(ray):
     hit_point = ti.Vector([0.0, 0.0, 0.0])
     hit_point_normal = ti.Vector([0.0, 0.0, 0.0])
     color = ti.Vector([0.0, 0.0, 0.0])
-    for n in range(max_depth):
-        is_hit, hit_point, hit_point_normal, front_face, material, color = scene.hit(Ray(scattered_origin, scattered_direction))
-        if is_hit:
-            if material == 0:
-                default_color = color
-                break
-            else:
-                # Compute the local color use Blinn-Phong model
-                hit_point_to_source = to_light_source(hit_point, ti.Vector([0, 5.4, -1]))
-                # Diffuse light
-                local_color = color * max(
-                    hit_point_to_source.dot(hit_point_normal) / (
-                            hit_point_to_source.norm() * hit_point_normal.norm()),
-                    0.0)
-                if material == 1:
-                    diffuse_color_buffer = local_color
-                    # shadow test
-                    a, b, c, d, material_test, e = scene.hit(Ray(hit_point, hit_point_to_source))
-                    if material_test != 0:
-                        diffuse_color_buffer *= 0
-                    # break
-                elif material == 2 or material == 3 or material == 4:
-                    intensity = 0.0
-                    # Specular light
-                    H = (-(ray.direction.normalized()) + hit_point_to_source.normalized()).normalized()
-                    N_dot_H = max(H.dot(hit_point_normal.normalized()), 0.0)
-                    intensity = ti.pow(N_dot_H, 10)
-                    specular_color = intensity * color
-                    specular_color_buffer += 0.1 * specular_color + 0.1 * color
 
-                    # shadow test
-                    a, b, c, d, material_test, e = scene.hit(Ray(hit_point, hit_point_to_source))
-                    if material_test != 0:
-                        specular_color_buffer *= 0
+    for i in range(3):
+        if scattered_buffer_mask[i] == 1:
+            scattered_origin = scattered_origin_buffer[i]
+            scattered_direction = scattered_direction_buffer[i]
+            for n in range(max_depth):
+                is_hit, hit_point, hit_point_normal, front_face, material, color = scene.hit(Ray(scattered_origin, scattered_direction))
+                if is_hit:
+                    if material == 0:
+                        default_color = color
+                        break
+                    else:
+                        # Compute the local color use Blinn-Phong model
+                        hit_point_to_source = to_light_source(hit_point, ti.Vector([0, 5.4 - 3.0, -1]))
+                        # Diffuse light
+                        local_color = color * max(
+                            hit_point_to_source.dot(hit_point_normal) / (
+                                    hit_point_to_source.norm() * hit_point_normal.norm()),
+                            0.0)
+                        # Diffuse
+                        if material == 1:
+                            diffuse_color_buffer = local_color
+                            # Add shadow
+                            is_hit_source, hitted_dielectric_num, is_hitted_non_dielectric = scene.hit_shadow(
+                                Ray(hit_point, hit_point_to_source))
+                            if not is_hit_source:
+                                if is_hitted_non_dielectric:
+                                    # Add hard shadow
+                                    diffuse_color_buffer *= 0
+                                elif hitted_dielectric_num > 0:
+                                    # Add soft shadow if the obstacles are dielectric
+                                    diffuse_color_buffer *= ti.pow(0.5, hitted_dielectric_num)
+                            break
+                        else:
+                            intensity = 0.0
+                            # Specular light
+                            H = (-(scattered_direction.normalized()) + hit_point_to_source.normalized()).normalized()
+                            N_dot_H = max(H.dot(hit_point_normal.normalized()), 0.0)
+                            intensity = ti.pow(N_dot_H, 10)
+                            specular_color = intensity * color
+                            specular_color_buffer += 0.1 * specular_color + 0.9 * color
 
-                    scattered_direction = reflect(scattered_direction.normalized(), hit_point_normal)
-                    scattered_origin = hit_point
-                    # if scattered_direction.dot(hit_point_normal) < 0:
-                    #     break
+                            # Add shadow
+                            is_hit_source, hitted_dielectric_num, is_hitted_non_dielectric = scene.hit_shadow(
+                                Ray(hit_point, hit_point_to_source))
+                            if not is_hit_source:
+                                if is_hitted_non_dielectric:
+                                    # Add hard shadow
+                                    specular_color_buffer *= 0
+                                elif hitted_dielectric_num > 0:
+                                    # Add soft shadow if the obstacles are dielectric
+                                    specular_color_buffer *= ti.pow(0.5, hitted_dielectric_num)
+                            # Metal
+                            if material == 2 or material == 4:
+                                scattered_direction = reflect(scattered_direction.normalized(), hit_point_normal) # + 0.4 * random_in_unit_sphere()
+                                scattered_origin = hit_point
+                                if scattered_direction.dot(hit_point_normal) < 0:
+                                    break
+                            # Dieletric
+                            elif material == 3:
+                                refraction_ratio = 1.5
+                                if front_face:
+                                    refraction_ratio = 1 / refraction_ratio
+                                cos_theta = min(-scattered_direction.normalized().dot(hit_point_normal), 1.0)
+                                sin_theta = ti.sqrt(1 - cos_theta * cos_theta)
+                                # total internal reflection
+                                if refraction_ratio * sin_theta > 1.0: # or reflectance(cos_theta, refraction_ratio) > ti.random():
+                                    scattered_direction = reflect(scattered_direction.normalized(), hit_point_normal)
+                                else:
+                                    scattered_direction = refract(scattered_direction.normalized(), hit_point_normal, refraction_ratio)
+                                scattered_origin = hit_point
+            # scattered_direction_buffer[i] = scattered_direction
+            # scattered_origin_buffer[i] = scattered_origin
     default_color += 0.5 * specular_color_buffer + diffuse_color_buffer
     return default_color
 
