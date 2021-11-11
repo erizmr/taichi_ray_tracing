8ab851c9d0cb85cfc3ce7943fcebf6f440040643
Mingrui <mingrui.zhang@taichi.graphics>
2021-11-11 17:41:08 +0800

    minor clean

--- ray_tracing_models.py
+++ ray_tracing_models.py
@@ -167,7 +167,6 @@ class Camera:
         w = (self.lookfrom[None] - self.lookat[None]).normalized()
         u = (self.vup[None].cross(w)).normalized()
         v = w.cross(u)
-        self.cam_lower_left_corner[None] = ti.Vector([-half_width, -half_height, -1.0])
         self.cam_lower_left_corner[
             None] = self.cam_origin[None] - half_width * u - half_height * v - w
         self.cam_horizontal[None] = 2 * half_width * u
